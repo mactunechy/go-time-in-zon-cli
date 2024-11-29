9d66ca8fe61af73ecf4bc60d2ede4fc71f4e0979
Dellan M <dellan.muchengapadare@platform45.com>
2024-11-29 20:33:17 +0200

    first commit

---
+++ cmd/timezone.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+// timezoneCmd represents the timezone command
+var timezoneCmd = &cobra.Command{
+	Use:   "timezone",
+	Short: "Get the current time in a given timezone",
+	Long: `Get the current time in a given timezone.
+							This command takes one argument, the timezon you want to get the current time in.
+							it returns the current time in RFC1123 format.`,
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		timezone := args[0]
+		location, _ := time.LoadLocation(timezone)
+		dateFlag, _ := cmd.Flags().GetString("date")
+		var date string
+		if dateFlag != "" {
+			date = time.Now().In(location).Format(dateFlag)
+		} else {
+			date = time.Now().In(location).Format(time.RFC1123)[:10]
+		}
+		fmt.Printf("Current date in %v: %v\n", timezone, date)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(timezoneCmd)
+	timezoneCmd.PersistentFlags().String("date", "", "returns the date in a timezone in a specified format")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// timezoneCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// timezoneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
